e14fa51ec220c3a4f64f604a6ad186ccc5421100
babludangi6266 <babludangi2000@gmail.com>
2024-07-07 19:28:32 +0530

    Desktop view complete

--- src/App.js
+++ src/App.js
@@ -1,40 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import { CssBaseline, ThemeProvider, createTheme, Box } from '@mui/material';
-// import Sidebar from './Components/SideBar';
-// import Login from './Components/Login';
-// import Dashboard from './Components/DashBoard';
-// import CreateProject from './Components/CreateProject';
-// // import Page2 from './Page2';
-// // import Page3 from './Page3';
-
-// const theme = createTheme();
-
-// function App() {
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <CssBaseline />
-//       <Router>
-//         <div style={{ display: 'flex' }}>
-//           <Sidebar />
-//           <Box component="main" sx={{ flexGrow: 1, p: 3, marginLeft: 30 }}>
-//             <Routes>
-              
-//               <Route path="/DashBoard" element={<Dashboard />} />
-//               <Route path="/" element={<Login />} />
-//               <Route path="/CreateProject" element={<CreateProject />} />
-//               {/* <Route path="/page1" element={<Page1 />} />
-//               <Route path="/page2" element={<Page2 />} />
-//               <Route path="/page3" element={<Page3 />} /> */}
-//             </Routes>
-//           </Box>
-//         </div>
-//       </Router>
-//     </ThemeProvider>
-//   );
-// }
-
-// export default App;
 
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
@@ -44,8 +7,7 @@ import Login from './Components/Login';
 import Dashboard from './Components/DashBoard';
 import CreateProject from './Components/CreateProject';
 import ProjectListing from './Components/ProjectListing';
-// import Page2 from './Page2';
-// import Page3 from './Page3';
+
 
 const theme = createTheme();
 
@@ -53,7 +15,7 @@ function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   const handleLogin = () => {
-    setLoggedIn(true); // Update state to indicate logged-in status
+    setLoggedIn(true); 
   };
 
   return (
